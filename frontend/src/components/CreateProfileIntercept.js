1e6ab0849080fd2e7258eab2b7aeb19630a72821
Rachel C <rachel.coppens@gmail.com>
2020-11-15 08:32:26 -0500

    update child welcome name

--- frontend/src/components/CreateProfileIntercept.js
+++ frontend/src/components/CreateProfileIntercept.js
@@ -8,7 +8,7 @@ const CreateProfileIntercept = ({ profileIntercept }) => {
       <div className="container">
         <div className="row">
           <div className="col-md-7">
-            <h1>Welcome, [child's name!]</h1>
+            <h1>Welcome, Cody!</h1>
             <p>Thank you for filling out your profile. We need a few more pieces of detailed information from you before you can start using this website. In order to do that, please book an appointment with us so we can build the rest of your profile.</p>
             <button type="button" onClick= {(e) => profileIntercept()} className="btn btn-danger">Book an appointment with us</button>
             <p>Once approved, you will receive an email notification from us that will allow you to navigate the rest of this website. <span className="non-emphasis-bold">Thank you for your patience!</span></p>
