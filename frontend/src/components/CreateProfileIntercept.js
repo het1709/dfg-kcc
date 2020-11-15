d3d54c4569237c2361b87e66772d2d0209a18f6d
Amie Everett <amie.everett@gmail.com>
2020-11-15 10:22:00 -0500

    Feat: add calendy to apt booking button

--- frontend/src/components/CreateProfileIntercept.js
+++ frontend/src/components/CreateProfileIntercept.js
@@ -10,7 +10,9 @@ const CreateProfileIntercept = ({ profileIntercept }) => {
           <div className="col-md-7">
             <h1>Welcome, Cody!</h1>
             <p>Thank you for filling out your profile. We need a few more pieces of detailed information from you before you can start using this website. In order to do that, please book an appointment with us so we can build the rest of your profile.</p>
-            <button type="button" onClick= {(e) => profileIntercept()} className="btn btn-danger">Book an appointment with us</button>
+            <a href="https://calendly.com/power-movement" target="_blank">
+              <button type="button" className="btn btn-danger">Book an appointment with us</button>
+            </a>
             <p>Once approved, you will receive an email notification from us that will allow you to navigate the rest of this website. <span className="non-emphasis-bold">Thank you for your patience!</span></p>
           </div>
           <div className="row">
