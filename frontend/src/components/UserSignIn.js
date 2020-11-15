21c52500f12b218d780d57b2ed66b1229bffeb2c
Rachel C <rachel.coppens@gmail.com>
2020-11-15 14:24:39 -0500

    remove functions

--- frontend/src/components/UserSignIn.js
+++ frontend/src/components/UserSignIn.js
@@ -10,13 +10,13 @@ const UserSignIn = () => {
                   <div className="row">
                     <div className="form-group col-md-12">
                       <label htmlFor="exampleInputEmail1">Email address:</label>
-                      <input type="text" onChange={(e) => onChangeForm(e)} className="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="user@domain.com" />
+                      <input type="text" className="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="user@domain.com" />
                     </div>
                   </div>
                   <div className="row">
                     <div className="form-group col-md-12">
                       <label htmlFor="exampleInputPassword1">Password:</label>
-                      <input type="password" onChange={(e) => onChangeForm(e)} className="form-control" name="password" id="password" placeholder="" />
+                      <input type="password" className="form-control" name="password" id="password" placeholder="" />
                     </div>
                   </div>
                 <a href="/dashboard">
