e5da894348ff74ef949514b951b2ffb621e5aae8
Rachel C <rachel.coppens@gmail.com>
2020-11-15 12:14:17 -0500

    about us page content

--- frontend/src/components/AboutUs.js
+++ frontend/src/components/AboutUs.js
@@ -4,8 +4,28 @@ const AboutUs = () => {
     return(
         <div className="container">
             <div className="row">
-              <h1>About Us</h1>
-              <p>Hello World!</p>
+                <div className="col-md-12">
+                    <h1>POWER Movement (Pediatric Oncology Website Empowering Resilience) through Movement</h1>
+                    <h2>About Us</h2>
+                    <p>Research has demonstrated that physical activity is an effective tool to help diminish the side-effects of cancer and its treatment and improve quality of life. Physical activity helps children with cancer to regain strength, endurance, bone density, maintain healthy weight, improve immune system recovery and brain development.</p>
+                    <h3>What is POWER Movement?</h3>
+                    <p>POWER Movement is a social platform that promotes Movement in children/teens affected by cancer.</p>
+                    <p>The purpose of the POWER Movement website is to increase awareness about the benefits of physical activity in children and adolescents affected by cancer. The website aims to increase accessibility of physical activity, so that participants, regardless of location (rural communities, urban areas, or in hospital settings) can engage in physical activity. The goal is to motivate children and adolescents affected by cancer and their families to MOVE more every day, and connect them to encourage them to interact with peers.</p>
+                    <p>The website is designed to be fun and engaging. POWER Movement encourages the entire family to get moving! As soon as you login and create your profile, you will have the option to choose an avatar. Next,  book an appointment with a qualified exercise professional and get ready to MOVE! So, what are you waiting for? Let’s GET MOVING!</p>
+                    <h3>Who we are?</h3>
+                    <p>The POWER Movement website, brought to you by Kids Cancer Care, was created in collaboration with an international, multidisciplinary team of experts specializing in pediatric oncology, led by Dr. Carolina Chamorro Vina, PhD and Mackenzie Murawsky, RN MScN(c).  The activities on the website are evidence based, and their development was informed by the team’s expertise.</p>
+                    <p>Dr. Carolina Chamorro Vina, PhD is a pediatric cancer and exercise specialist and coordinator for Kids Cancer Care’s <b><u>P</u></b>ediatric Cancer Patients and Survivors <b><u>E</u></b>ngaging in <b><u>E</u></b>xercise for <b><u>R</u></b>ecovery (PEER) program.</p>
+                    <p>Mackenzie Murawsky is the Kids Cancer Care Nursing Manager.</p>
+                    <p>The creation of this website was made possible thanks to the Digital for Good Tech Jam Grant offered by Capital One, and through the support of the Calgary Flames Foundation.</p>
+                    <h3>How to use this website:</h3>
+                    <p>The website focuses on 3 main functions to help your child and family MOVE MORE:</p>
+                    <ol>
+                        <li><span class="non-emphasis-bold">Initial assessment:</span> Targeted screening tools and daily questionnaires will help us to understand more about your child’s health and wellbeing, their physical activity levels, and cancer related factors. This will help us to determine which exercises are best suited to your child’s needs.</li>
+                        <li><span class="non-emphasis-bold">Movement recommendation(s):</span> Based on your child's unique condition, and any cancer related side effects they may be experiencing, the website will link them to customized physical activity options. Each time your child engages in physical activity, they will earn rewards!</li>
+                        <li><span className="non-emphasis-bold">Coming Soon!<br />Connect and interact with peers:</span> We plan to create a friend suggestion tool to connect your child with other children and youth who are also navigating their cancer journey. This will help to promote peer support and encourage your child to keep moving!</li>
+                    </ol>
+                    <p><a href={"https://www.kidscancercare.ab.ca/"}>Visit the Kids Cancer Care main website</a>.</p>
+                </div>
             </div>    
         </div>
     );
