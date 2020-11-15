1958b37f6e874b9dcdfef2115dccba287f277cba
Amie Everett <amie.everett@gmail.com>
2020-11-15 14:19:58 -0500

    feat: add social media icons

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -14,11 +14,20 @@ export const Footer = () => {
               <p>Copyright &copy; 2020 Kids Cancer Care Foundation of Alberta. All rights reserved</p>
             </div>
             <div className="footer-right col-md-6">
-              {/* <i class="fi-social-instagram"></i>
-              <i class="fi-social-instagram"></i>
-              <i class="fi-social-twitter"></i> */}
-              <p><a href={"#"} className="privacy-link">Privacy Policy</a></p>
-              <p><a href={"#"} className="terms-of-use-link">Terms of Use</a></p>
+              <div className="row social">
+                {/* <i class="fi-social-instagram"></i>
+                <i class="fi-social-instagram"></i>
+                <i class="fi-social-twitter"></i> */}
+                <i class="fa fa-instagram" aria-hidden="true"></i>
+                <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+                <i class="fa fa-youtube-play" aria-hidden="true"></i>
+                <i class="fa fa-facebook-square" aria-hidden="true"></i>
+                <i class="fa fa-twitter" aria-hidden="true"></i>
+              </div>
+              <div className="row">
+                <p><a href={"#"} className="privacy-link">Privacy Policy</a></p>
+                <p><a href={"#"} className="terms-of-use-link">Terms of Use</a></p>
+              </div>
             </div>
           </div>
         </footer>
